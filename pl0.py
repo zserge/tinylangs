243aa2d6232134c574dde59f9aa52cba436735ca
Juho Eerola <juntuu@iki.fi>
2024-09-22 23:36:29 +0300

    PL/0: if as expression with and

--- pl0.py
+++ pl0.py
@@ -67,8 +67,7 @@ def eval(node, scope=(None, {}, {})):
     elif node[0] == "begin": [eval(n, scope) for n in node[1]]
     elif node[0] == "read": env = find(node[1], 1); env[node[1]] = int(input("> "))
     elif node[0] == "op": return {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.floordiv, "<": operator.lt, ">": operator.gt, "=": operator.eq}[node[1]](eval(node[2], scope), eval(node[3], scope))
-    elif node[0] == "if":
-        if eval(node[1], scope): eval(node[2], scope)
+    elif node[0] == "if": _ = eval(node[1], scope) and eval(node[2], scope)
     elif node[0] == "while":
         while eval(node[1], scope): eval(node[2], scope)
     elif node[0] == "write": print(eval(node[1], scope))
